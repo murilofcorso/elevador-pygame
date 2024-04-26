26335b5898605b18c873b7e6234cd291155c83e6
Murilo Corso <murilofcorso@gmail.com>
2024-04-26 03:32:43 -0300

    animacao de fechamento de porta

--- elevador.py
+++ elevador.py
@@ -31,7 +31,8 @@ class Elevador(pygame.sprite.Sprite):
                 self.rect.y += 2
             else:
                 self.status = "parado"
-                self.abrir_porta()
+                if len(self.chamados) > 0:
+                    self.abrir_porta()
 
         if self.rect.bottom in self.andares.keys():
             self.andar_atual = self.andares[self.rect.bottom]
@@ -39,29 +40,36 @@ class Elevador(pygame.sprite.Sprite):
             
     def abrir_porta(self):
         if self.status == "parado":
-            self.status = "porta_aberta"
+            self.status = "porta_abrindo"
 
-        # if self.status == "porta_aberta":
+        # if self.status == "porta_abrindo":
 
 
     def desenhar(self):
         self.image.fill("black")
-        if self.status != "porta_aberta":
+        if self.status in ("parado", "movendo"):
             self.largura_porta = self.largura/2
             self.pos_porta_direita = self.largura_porta
         else:
-            self.largura_porta -= 1
-            self.pos_porta_direita += 1
-            if self.largura_porta <= 0:
-                self.fechar_porta()
+            if self.status == "porta_abrindo":
+                self.largura_porta -= 1
+                self.pos_porta_direita += 1
+                if self.largura_porta <= 0:
+                    self.fechar_porta()
+            else:
+                self.largura_porta += 1
+                self.pos_porta_direita -= 1
+                if self.largura_porta >= self.largura/2:
+                    self.status = "parado"
+                    self.chamados.pop(0)
 
         pygame.draw.rect(self.image, (255, 255, 255), (0, 0, self.largura_porta, self.altura))
         pygame.draw.rect(self.image, (255, 255, 255), (self.pos_porta_direita, 0, self.largura_porta, self.altura))
 
 
     def fechar_porta(self):
-        if self.status == "porta_aberta":
-            self.status = "parado"
+        if self.status == "porta_abrindo":
+            self.status = "porta_fechando"
 
 
     def adicionar_chamado(self, andar):
@@ -74,7 +82,6 @@ class Elevador(pygame.sprite.Sprite):
             if self.andar_atual != self.chamados[0]:
                 return self.chamados[0]
             else:
-                self.chamados.pop(0)
                 return self.andar_atual
         else:
             return self.andar_atual
