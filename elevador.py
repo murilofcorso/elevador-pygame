2d18eb84ec0a3c5ec4cf56a2b0eaa656313a6245
Murilo Corso <murilofcorso@gmail.com>
2024-04-30 15:42:08 -0300

    criacao do metodo cooldowns

--- elevador.py
+++ elevador.py
@@ -20,6 +20,9 @@ class Elevador(pygame.sprite.Sprite):
         self.rect.bottomleft = (self.tela.get_width()/2 - self.largura/2, self.tela.get_height() - (self.andar_atual * self.altura))
         self.chamados = []
 
+        self.fechou = False
+        self.abriu = False
+
 
     def mover(self, destino):
         if self.status == "parado":
@@ -54,13 +57,17 @@ class Elevador(pygame.sprite.Sprite):
                 self.largura_porta -= 1
                 self.pos_porta_direita += 1
                 if self.largura_porta < -1:
-                    pygame.time.delay(2000)
+                    self.status = "porta_aberta"
+                    self.abriu = True
+                    self.abertura_tick = pygame.time.get_ticks()
                     self.fechar_porta()
             elif self.status == "porta_fechando":
                 self.largura_porta += 1
                 self.pos_porta_direita -= 1
                 if self.largura_porta >= self.largura/2:
-                    self.status = "parado"
+                    self.status = "porta_fechada"
+                    self.fechou = True
+                    self.fechamento_tick = pygame.time.get_ticks()
                     self.chamados.pop(0)
 
         pygame.draw.rect(self.image, (255, 255, 255), (0, 0, self.largura_porta, self.altura))
@@ -70,6 +77,20 @@ class Elevador(pygame.sprite.Sprite):
     def fechar_porta(self):
         if self.status == "porta_abrindo":
             self.status = "porta_fechando"
+
+
+    def cooldowns(self):
+        self.tick_atual = pygame.time.get_ticks()
+        # cooldown 
+        if self.fechou:
+            if self.tick_atual - self.fechamento_tick > 1200:
+                self.fechou = False
+                self.status = "parado"
+
+        if self.abriu:
+            if self.tick_atual - self.abertura_tick > 1200:
+                self.abriu = False
+                self.status = "porta_fechando"
 
 
     def adicionar_chamado(self, andar):
@@ -84,9 +105,10 @@ class Elevador(pygame.sprite.Sprite):
             else:
                 return self.andar_atual
         else:
-            return self.andar_atual
+            return 0
 
 
     def update(self):
+        self.cooldowns()
         self.desenhar()
         self.mover(self.retornar_proximo_chamado())
